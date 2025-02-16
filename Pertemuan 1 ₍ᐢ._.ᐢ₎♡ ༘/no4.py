56f429e7e3e4fdfe631a7c7636389ebf263389ec
Tanaya Nindya Pradipta <tanayanindyapradipta2006@mail.ugm.ac.id>
2025-02-16 09:39:17 +0700

    Update no4.py

--- Pertemuan 1 ₍ᐢ._.ᐢ₎♡ ༘/no4.py
+++ Pertemuan 1 ₍ᐢ._.ᐢ₎♡ ༘/no4.py
@@ -9,5 +9,9 @@ elif angka % 2 == 0:
     print("Bukan Angka Prima")
 elif angka % 3 == 0:
     print("Bukan Angka Prima")
+elif angka % 5 == 0:
+    print("Bukan Angka Prima")
+elif angka < 1:
+    print("Bukan Angka Prima")
 else:
     print("Angka Prima")
